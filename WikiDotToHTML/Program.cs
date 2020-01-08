f6ceec18e8d451a882482c962d4cd5209d07c4f4
shortstop4313@gmail.com <shortstop4313@gmail.com>
2020-01-07 21:53:22 -0500

    More adjstmest

--- WikiDotToHTML/Program.cs
+++ WikiDotToHTML/Program.cs
@@ -7,15 +7,26 @@ namespace WikiDotToHTML
 {
     class Program
     {
-        static readonly string StTag = "Niv:\t##666666|";
-        static readonly string YaraTag = "YaraSherred:\t##666666|";
-        static readonly string KaiTag = "Kai_Tukov:\t##110481|";
+        //static readonly string StTag = "ST4313:\t##107010|";
+        //static readonly string YaraTag = "YaraSherred:\t##666666|";
+        //static readonly string KaiTag = "Kai_Tukov:\t##110481|";
+        //static readonly string GuylianTag = "Guylian:  ##208020| ";
+        ////static readonly string DamianTag = "DamianStark:\t##208020|";
+        //static readonly string DamianTag = "DamianStark:\t##800000|";
+        //static readonly string BlacksmithTag = "BlackSmithST:\t##008000|";
+        //static readonly string BlacksmithTag2 = "BlacksmithST:\t##800000|";
+        //static readonly string endLineTag = "##";
+
+        static readonly string StTag = "YaraST:  ##666666| ";
+        static readonly string YaraTag = "YaraSherred:  ##666666| ";
+        static readonly string KaiTag = "Kai_Tukov:  ##110481| ";
+        static readonly string GuylianTag = "Guylian:  ##208020| ";
         //static readonly string DamianTag = "DamianStark:\t##208020|";
-        static readonly string DamianTag = "DamianStark:\t##800000|";
-        static readonly string BlacksmithTag = "BlackSmithST:\t##008000|";
-        static readonly string BlacksmithTag2 = "BlacksmithST:\t##800000|";
-        static readonly string GuylianTag = "Guylian:\t##336633|";
+        static readonly string DamianTag = "DamianStark:  ##800000| ";
+        static readonly string BlacksmithTag = "BlackSmithST:  ##008000| ";
+        static readonly string BlacksmithTag2 = "BlacksmithST:  ##800000| ";
         static readonly string endLineTag = "##";
+
 
         static void Main(string[] args)
         {
@@ -56,19 +67,21 @@ namespace WikiDotToHTML
 
         private static string ProcessLine(string line)
         {
-            if ((line.Contains("\t((") && (line.EndsWith("))##") || line.EndsWith(")) ## "))) || line.Contains("\t[[") && (line.EndsWith("]]##") || line.EndsWith("]] ## "))) 
+            if (line.Contains("\t((") && (line.EndsWith("))##") || line.EndsWith(")) ## ")))
             {
                 line = string.Empty;
             }
             else
             {
                 line = ReservedCharacterChangePass(line);
-                line = line.Replace(StTag, "<p style='color:#666666;'><span style='font-weight: bold; color:#000000;'>NivST:\t</span>");
+                //line = line.Replace(StTag, "<p style='color:#107010;'><span style='font-weight: bold; color:#000000;'>ST4313:\t</span>");
+                line = line.Replace(StTag, "<p style='color:#666666;'><span style='font-weight: bold; color:#000000;'>YaraST:\t</span>");
+
                 line = line.Replace(YaraTag, "<p style='color:#666666;'><span style='font-weight: bold; color:#000000;'>YaraSherred:\t</span>");
                 line = line.Replace(KaiTag, "<p style='color:#110481;'><span style='font-weight: bold; color:#000000; font-family: Lucida Console, Monaco, monospace; letter - spacing: 0.07em;'>KaiTukov:\t</span>");
+                line = line.Replace(GuylianTag, "<p style='color:#208020;'><span style='font-weight: bold; color:#000000;'>Guylian:\t</span>");
                 line = line.Replace(DamianTag, "<p style='color:#800000;'><span style='font-weight: bold; color:#000000;'>DamianStark:\t</span>");
                 //line = line.Replace(DamianTag, "<p style='color:#208020;'><span style='font-weight: bold; color:#000000;'>DamianStark:\t</span>");
-                line = line.Replace(GuylianTag, "<p style='color:#336633;'><span style='font-weight: bold; color:#000000;'>Guylian:\t</span>");
                 line = line.Replace(BlacksmithTag, "<p style='color:#800000;'><span style='font-weight: bold; color:#000000;'>BlackSmith:\t</span>");
                 line = line.Replace(BlacksmithTag2, "<p style='color:#800000;'><span style='font-weight: bold; color:#000000;'>BlackSmith:\t</span>");
                 line = line.Replace(endLineTag, "</p>");
